9cc0d6ac52ec4b0dbcc5a1aee80d0b1ae1a92077
d-speedster <mrspeed717@gmail.com>
2025-07-19 23:08:37 +0330

    update movies page

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ const path = require('path');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+
+app.get('/movies', (req, res, next) => {
+    res.render('movies/main')
+})
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('layout/main')
 });
-
-
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
